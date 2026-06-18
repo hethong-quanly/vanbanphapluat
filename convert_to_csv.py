09d9c9919bda02f82bcd4c6e3668db5579a77761
hethong-quanly <mrphuc2009@gmail.com>
2026-06-18 10:24:27 +0700

    Update convert_to_csv.py

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -2,8 +2,8 @@ import json
 import csv
 
 # Đường dẫn file JSON của bạn
-json_file = r"D:\vbpl_database.json"
-csv_file = r"D:\documents.csv"
+json_file = r"D:\vbpl_database.json"     
+csv_file  = r"D:\documents.csv"   
 
 with open(json_file, 'r', encoding='utf-8') as f:
     data = json.load(f)
